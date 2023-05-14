263be646962dcf4cadbb41f82b45ff71900357d4
Dimas <dimashpt@gmail.com>
2023-05-14 09:44:11 +0700

    add env and initial telegraf

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+
+const bot = new Telegraf(process.env.BOT_TOKEN!);
+bot.start((context) => context.reply('Welcome'));
+bot.help((context) => context.reply('Send me a sticker'));
+bot.on(message('sticker'), (context) => context.reply('👍'));
+bot.hears('hi', (context) => context.reply('Hey there'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
